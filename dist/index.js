1f26f15e213522d3571795eac2931ae3418e518e
jatinsingh-tomar <js8971105@gmail.com>
2023-07-31 15:28:49 +0545

    action

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
-let message = 'Hello World';
-console.log(message);
+let message = "Hello World";
+const a = 1;
+console.log(`${message}  this is cool ${a}`);
 //# sourceMappingURL=index.js.map
